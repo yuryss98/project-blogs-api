b745d1f5b5fb9b7b8b26d1042a6d613b52f8822d
Yury Stolarz <yurysscareer@gmail.com>
2022-12-03 15:09:57 -0300

    refactor: services/validations/schemas.js

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -1,44 +1,49 @@
 const Joi = require('joi');
 
-const customMessage = 'Some required fields are missing';
+const messageForRequiredFields = 'Some required fields are missing';
+const messageToTextFields = '{#label} must be the type text';
+const customMessage = {
+  'any.required': messageForRequiredFields,
+  'string.empty': messageForRequiredFields,
+  'string.base': messageToTextFields,
+  'array.min': messageForRequiredFields,
+};
+
+const categoryIds = Joi.array().items(Joi.number()).min(1).required();
+const content = Joi.string().min(1).required();
+const displayName = Joi.string().min(8).required();
+const email = Joi.string().email().min(1).required();
+const image = Joi.string();
+const name = Joi.string().min(5).required();
+const password = Joi.string().min(6).required();
+const title = Joi.string().min(1).required();
 
 const loginSchema = Joi.object({
-  email: Joi.string().min(1).required(),
-  password: Joi.string().min(1).required(),
-}).required().messages({
-  'any.required': customMessage,
-  'string.empty': customMessage,
-  'string.base': '{#label} must be the type text',
-});
+  email,
+  password,
+}).required().messages(customMessage);
 
 const createUserSchema = Joi.object({
-  displayName: Joi.string().min(8).required(),
-  email: Joi.string().email().min(1).required(),
-  password: Joi.string().min(6).required(),
-  image: Joi.string(),
-});
+  displayName,
+  email,
+  password,
+  image,
+}).required();
 
 const createCategorySchema = Joi.object({
-  name: Joi.string().min(5).required(),
-});
+  name,
+}).required();
 
 const createBlogPostSchema = Joi.object({
-  title: Joi.string().min(1).required(),
-  content: Joi.string().min(1).required(),
-  categoryIds: Joi.array().items(Joi.number()).min(1).required(),
-}).required().messages({
-  'any.required': customMessage,
-  'string.empty': customMessage,
-  'array.min': customMessage,
-});
+  title,
+  content,
+  categoryIds,
+}).required().messages(customMessage);
 
 const updateBlogPostSchema = Joi.object({
-  title: Joi.string().min(1).required(),
-  content: Joi.string().min(1).required(),
-}).required().messages({
-  'any.required': customMessage,
-  'string.empty': customMessage,
-});
+  title,
+  content,
+}).required().messages(customMessage);
 
 module.exports = {
   loginSchema,
